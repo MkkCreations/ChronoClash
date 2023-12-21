a031360f8d49783bd026107668cd1eb70499273a
Khaffou Khaffou Mohamed <khaffoum@gmail.com>
2023-12-22 00:27:41 +0100

    created unit class and playercontroller class

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+using Photon.Realtime;
+
+public class PlayerController : MonoBehaviourPun
+{
+    public Player photonPlayer;
+    public string[] unitsToSpawn;
+    public Transform[] spawnPoints;
+
+    public List<Unit> units = new List<Unit>();
+    public Unit selectedUnit;
+
+    public static PlayerController me;
+    public static PlayerController enemy;
+
+    [PunRPC]
+    void Initialize(Player player)
+    {
+        photonPlayer = player;
+
+        // If is our local player, spawn the units
+
+        if (player.IsLocal)
+        {
+            me = this;
+        }
+        else
+        {
+            enemy = this;
+        }
+
+        // Set the UI player text
+    }
+
+    void SpawnUnits()
+    {
+        for (int x =0; x < unitsToSpawn.Length; ++x)
+        {
+            GameObject unit = PhotonNetwork.Instantiate(unitsToSpawn[x], spawnPoints[x].position, Quaternion.identity);
+            unit.GetPhotonView().RPC("Initialize", RpcTarget.Others, false);
+            unit.GetPhotonView().RPC("Initialise", photonPlayer, true);
+        }
+    }
+    
+}
---
+++ Assets/Scripts/Unit.cs
@@ -0,0 +1,114 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using Photon.Pun;
+
+public class Unit : MonoBehaviourPun
+{
+    public int curHp;
+    public int maxHp;
+    public float moveSpeed;
+    public int minDamage;
+    public int maxDamage;
+
+    public int maxMoveDistance;
+    public int maxAttackDistance;
+
+    public bool usedThisTurn;
+
+    public GameObject selectedVisual;
+    public SpriteRenderer spriteVisual;
+
+    [Header("UI")]
+    public Image healthFillImage;
+
+    [Header("Sprite Variants")]
+    public Sprite leftPlayerSprite;
+    public Sprite rightPlayerSprite;
+
+    [PunRPC]
+    void Initialize(bool isMaine)
+    {
+        healthFillImage.fillAmount = 1.0f;
+
+        // Set sprite variant
+        spriteVisual.sprite = transform.position.x < 0 ? leftPlayerSprite : rightPlayerSprite;
+
+        // Rotate the unit
+        spriteVisual.transform.up = transform.position.x < 0 ? Vector3.left : Vector3.right;
+    }
+
+
+    public bool CanSelect()
+    {
+        if (usedThisTurn) return false;
+        else return true;
+    }
+
+    public bool CanMove(Vector3 movePos)
+    {
+        if (Vector3.Distance(transform.position, movePos) <= maxMoveDistance) return true;
+        else return false;
+    }
+
+    public bool CanAttack(Vector3 attackPos)
+    {
+        if (Vector3.Distance(transform.position, attackPos) <= maxAttackDistance) return true;
+        else return false;
+    }
+
+    public void ToggleSelect(bool selected)
+    {
+        selectedVisual.SetActive(selected);
+    }
+
+    public void Move(Vector3 targetPos)
+    {
+        usedThisTurn = true;
+
+        Vector3 dir = (transform.position - targetPos).normalized;
+        spriteVisual.transform.up = dir;
+
+        StartCoroutine(MoveOverTime());
+
+        IEnumerator MoveOverTime()
+        {
+            while(transform.position != targetPos)
+            {
+                transform.position = Vector3.MoveTowards(transform.position, targetPos, moveSpeed * Time.deltaTime);
+                yield return null;
+            }
+        }
+    }
+
+    public void Attack(Unit unitToAttack)
+    {
+        usedThisTurn = true;
+        // unitToAttack.photonView.RPC("TakeDamage", )
+    }
+
+    [PunRPC]
+    void TakeDamage(int damage)
+    {
+        curHp -= damage;
+
+        if (curHp <= 0) photonView.RPC("Die", RpcTarget.All);
+        else
+        {
+            photonView.RPC("UpdateHealthBar", RpcTarget.All, (float)curHp / (float)maxHp);
+        }
+    }
+
+    [PunRPC]
+    void UpdateHealthBar(float fillAmount)
+    {
+        healthFillImage.fillAmount = fillAmount;
+    }
+
+    [PunRPC]
+    void Die()
+    {
+        
+    }
+}
