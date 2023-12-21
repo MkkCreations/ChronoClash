6ba18c410cb131c0316fb1a06da79bbc549e8d47
Khaffou Khaffou Mohamed <khaffoum@gmail.com>
2023-12-21 22:29:04 +0100

    created a basic menu

---
+++ Assets/Scripts/NetworkManager.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+using Photon.Realtime;
+
+public class NetworkManager : MonoBehaviourPunCallbacks
+{
+    // instance
+    public static NetworkManager instance;
+
+    private void Awake()
+    {
+        instance = this;
+        DontDestroyOnLoad(gameObject);
+    }
+
+    private void Start()
+    {
+        // connect to the server
+        PhotonNetwork.ConnectUsingSettings();
+    }
+
+    // Joins a random room or creates a new room
+    public void CreateOrJoinRoom()
+    {
+        if (PhotonNetwork.CountOfRooms > 0)
+            PhotonNetwork.JoinRandomRoom();
+        else
+        {
+            RoomOptions options = new RoomOptions();
+            options.MaxPlayers = 2;
+
+            PhotonNetwork.CreateRoom(null, options);
+        }
+    }
+
+    // change the scene using Photon's system
+    public void ChangeScene(string sceneName)
+    {
+        PhotonNetwork.LoadLevel(sceneName);
+    }
+
+}
