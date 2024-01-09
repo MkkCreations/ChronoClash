10c66c4232ae7557ddb1157ed6f7069bc2f0aa03
tijou allan <tijouallan@gmail.com>
2024-01-09 12:10:18 +0100

    toute les interfaces de login/register

--- Assets/Scripts/Panel/PanelManager.cs
+++ Assets/Scripts/Panel/PanelManager.cs
@@ -7,6 +7,7 @@ public class PanelManager : MonoBehaviour
     public static PanelManager instance;
     public Login loginPanel;
     public Home homePanel;
+    public Register registerPanel;
 
     private void Awake()
     {
@@ -21,13 +22,38 @@ public class PanelManager : MonoBehaviour
 
     public void GoHome()
     {
-        loginPanel.gameObject.SetActive(false);
-        homePanel.gameObject.SetActive(true);
+        if (loginPanel.gameObject.activeSelf )
+        {
+            loginPanel.gameObject.SetActive(false);
+            homePanel.gameObject.SetActive(true);
+        }
+
+        else if (registerPanel.gameObject.activeSelf)
+        {
+            registerPanel.gameObject.SetActive(false);
+            homePanel.gameObject.SetActive(true);
+        }
     }
 
     public void GoLogin()
     {
-        loginPanel.gameObject.SetActive(true);
-        homePanel.gameObject.SetActive(false);
+        if (homePanel.gameObject.activeSelf)
+        {
+            loginPanel.gameObject.SetActive(true);
+            homePanel.gameObject.SetActive(false);
+        }
+        if (registerPanel.gameObject.activeSelf)
+        {
+            loginPanel.gameObject.SetActive(true);
+            registerPanel.gameObject.SetActive(false);
+        }
     }
+
+    public void GoRegister()
+    {
+        registerPanel.gameObject.SetActive(true);
+        loginPanel.gameObject.SetActive(false);
+        
+    }
+
 }
